03a226ff28fef4a8cc7dd59ed0cdd4a37d59db40
Matthew Horn <matthewhorn1995@gmail.com>
2025-01-03 17:33:55 -0800

    Create tasks to load raw data into Snowflake

--- dags/CovidPipeDag.py
+++ dags/CovidPipeDag.py
@@ -1,23 +1,24 @@
 from datetime import timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime
-
-
 import sys
+
 # Include the airflow directory in the python path to access the helper functions
 sys.path.insert(0, '/Users/matthewmac/airflow')
 
-
 # Import helper functions for tasks
-from helper_scripts.print_helper import helper_print_task
 from helper_scripts.api_fetch import main as api_fetch
+
+#
+#
+#
 
 # Set the default arguments for the DAG
 default_args = {
     'owner': 'Matthew Horn',
-    'start_date': datetime.now(),
+    'start_date': datetime(2025, 1, 1),
     'retries': 1,
     'retry_delay': timedelta(minutes=2)
 }
@@ -33,39 +34,67 @@ dag = DAG(
 # Task to fetch raw data from api's and store locally in csv files
 fetch_raw_data = PythonOperator(
     task_id='fetch_raw',
-    python_callable=api_fetch
+    python_callable=api_fetch,
+    dag=dag
 )
 
 # Task to stage raw data
-stage_raw_data = SnowflakeOperator(
+stage_raw_data = BashOperator(
     task_id='stage_raw_data',
-    snowflake_conn_id='snowflake_default',
-    autocommit=True,
-    sql=f"PUT ~/airflow/data/raw/industrial_production_{datetime.now().date()}.csv @raw_data_stage",
-    database='CovidE2EPipDatabase',
-    schema='raw_data',
-    warehouse='COMPUTE_WH',
+    bash_command='snow stage copy ~/airflow/data/raw/industrial_production_{{ ds }}.csv @raw_data_stage',
     dag=dag
 )
 
-# SQL code to copy staged raw data into tables in raw_data schema
-upload_sql = f"""
-COPY INTO raw_data_schema.industrial_production
-FROM @raw_data_stage/industrial_production_{datetime.now().date()}.csv
-FILE_FORMAT = (TYPE = 'CSV')
-ON_ERROR = 'CONTINUE';
-"""
+# Task to create sql executable file daily to transfer daily raw data from the stage to tables
+create_load_sql_file = BashOperator(
+    task_id='create_load_sql_file',
+    bash_command="""
+    echo "COPY INTO CovidE2EPipeDatabase.raw_data.industrial_production
+    FROM @raw_data_stage
+    FILES = ('~/airflow/data/raw/industrial_production_{{ ds }}.csv')
+    FILE_FORMAT = ( TYPE='CSV' )
+    ON_ERROR = 'CONTINUE';
+    " > ~/airflow/helper_scripts/load_staged_data_{{ ds }}.sql
+    """,
+    dag=dag
+)
 
+# Task to stage sql executable file
+stage_sql_exe = BashOperator(
+    task_id='stage_sql',
+    bash_command='snow stage copy ~/airflow/helper_scripts/load_staged_data_{{ ds }}.sql',
+    dag=dag
+)
 
 # Task to insert staged raw data into tables
-insert_staged_raw_data = SnowflakeOperator(
+insert_staged_raw_data = BashOperator(
     task_id='insert_staged_raw_data',
-    snowflake_conn_id='snowflake_default',
-    sql=upload_sql,
-    database='CovidE2EPipDatabase',
-    schema='raw_data',
-    warehouse='COMPUTE_WH',
+    bash_command='snow stage execute @raw_data_stage/load_staged_data_{{ ds }}.sql',
     dag=dag
 )
 
-fetch_raw_data >> stage_raw_data >> insert_staged_raw_data
+# Task to clean up the sql executable from local storage and snowflake stage
+clean_up_sql_files = BashOperator(
+    task_id='clean_up_sql_exe',
+    bash_command='snow stage remove @raw_data_stage/load_staged_data_{{ ds }}.sql; \
+                  rm ~/airflow/helper_scripts/load_staged_data_{{ ds }}.sql',
+    dag=dag
+)
+
+# Task to archive old raw data files that have already been uploaded to snowflake
+archive_raw_data = BashOperator(
+    task_id='archive_raw_data',
+    bash_command='cd ~/airflow/data/raw && zip raw_data_{{ ds }}.zip *.csv \
+                    && mv raw_data_{{ ds }}.zip archived/',
+    dag=dag
+)
+
+# Define the pipeline task dependencies
+(
+    fetch_raw_data 
+    >> [stage_raw_data, create_load_sql_file] 
+    >> stage_sql_exe 
+    >> insert_staged_raw_data 
+    >> [clean_up_sql_files, archive_raw_data]
+
+)
